a7b3b587d9c0c78ef595f444c4842e33cf0313c9
wetdirtysponge <savtref@gmail.com>
2025-08-07 23:57:02 +0400

    Update and rename vite.config.ts to .nojekyll

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/stunning-eureka/', // 👈 очень важно!
-  plugins: [react()]
-})
